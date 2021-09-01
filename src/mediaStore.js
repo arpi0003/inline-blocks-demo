ec5b7f1bb0197f6caa259ddb93a1935a89beab87
Eddie Arpin <eddie.arpin@clowd-lab.com>
2021-09-01 12:17:35 -0500

    adding debug info to media store

--- src/mediaStore.js
+++ src/mediaStore.js
@@ -1,8 +1,32 @@
+import path from 'path';
+
 export default class DummyMediaStore {
   accept = '*';
-  async persist() {
-    alert('Cannot upload new files without a backend');
+
+  async persist(files) {
+    const uploaded = [];
+    console.log('media manager: files: ', files);
+    debugger;
+    for (const { file, directory } of files) {
+      let mediaPath = path.join(directory, file.name);
+      if (mediaPath.charAt(0) === '/') {
+        mediaPath = mediaPath.slice(1);
+      }
+      alert(`Media path is ${mediaPath}`);
+      // try {
+      //   const content = (await base64File(file)).toString().split(',')[1] // only need the data piece
+      //   const uploadResponse: GithubUploadResponse =
+      //     await this.githubClient.upload(mediaPath, content, 'Upload', true)
+      //
+      //   uploaded.push(contentToMedia(uploadResponse.content))
+      // } catch (e) {
+      //   console.warn('Failed to upload content to Github: ' + e)
+      // }
+    }
+
+    return uploaded
   }
+  
   async previewSrc(src) {
     return src;
   }
